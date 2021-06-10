260a05bc6f104a71e9e0dc4d7fbc21906760963f
mapfeiffer02 <60476207+mapfeiffer02@users.noreply.github.com>
2021-06-10 16:12:08 +0200

    Best answer to null when deleting best answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -64,8 +64,11 @@ class AnswerController extends Controller
      * @param  \App\Models\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete', $answer);
+
+        $answer->delete();
+        return back()->with('success', 'Ypur Answer has been removed.');
     }
 }
--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -30,7 +30,10 @@ class Answer extends Model
 
         static::created(function($answer) {
             $answer->question->increment('answers_count');
-            $answer->question->save();
+        });
+        
+        static::deleted(function($answer) {
+            $answer->question->decrement('answers_count');
         });
     }
     
@@ -43,5 +46,10 @@ class Answer extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
